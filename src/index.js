cd501ac642d5e3af4b4f99e1c4d504f321840738
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-07-08 13:21:09 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,59 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const skills = [
+  {
+    name: "HTML+CSS",
+    level: "advanced",
+    bgColor: "#2662EA",
+  },
+  {
+    name: "JavaScript",
+    level: "advanced",
+    bgColor: "#EFD81D",
+  },
+  {
+    name: "PHP",
+    level: "advanced",
+    bgColor: "#C3DCAF",
+  },
+  {
+    name: "React",
+    level: "beginner",
+    bgColor: "#E84F33",
+  },
+  {
+    name: "Redux",
+    level: "beginner",
+    bgColor: "#60DAFB",
+  },
+  {
+    name: "Vue",
+    level: "advanced",
+    bgColor: "#AD3A30",
+  },
+  {
+    name: "Laravel",
+    level: "intermediate",
+    bgColor: "purple",
+  },
+  {
+    name: "Bootstrap",
+    level: "advanced",
+    bgColor: "grey",
+  },
+  {
+    name: "MySQL",
+    level: "intermediate",
+    bgColor: "brown",
+  },
+  {
+    name: "Figma",
+    level: "beginner",
+    bgColor: "#FAAB00",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -36,24 +89,27 @@ function Intro() {
 function SkillList() {
   return (
     <ul className="skill-list">
-      <Skill name="HTML+CSS" bgColor="blue" />
-      <Skill name="JavaScript" bgColor="pink" />
-      <Skill name="PHP" bgColor="purple" />
-      <Skill name="React" bgColor="yellow" />
-      <Skill name="Redux" bgColor="green" />
-      <Skill name="Vue" bgColor="lightgrey" />
-      <Skill name="Laravel" bgColor="grey" />
-      <Skill name="Bootstrap" bgColor="orange" />
-      <Skill name="MySQL" bgColor="brown" />
-      <Skill name="Figma" bgColor="red" />
+      {skills.map((skill) => (
+        <Skill
+          name={skill.name}
+          level={skill.level}
+          bgColor={skill.bgColor}
+          key={skill.name}
+        />
+      ))}
     </ul>
   );
 }
 
-function Skill(props) {
+function Skill({ name, level, bgColor }) {
   return (
-    <li className="skill" style={{ backgroundColor: props.bgColor }}>
-      {props.name}
+    <li className="skill" style={{ backgroundColor: bgColor }}>
+      {name}
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
     </li>
   );
 }
